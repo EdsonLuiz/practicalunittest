fc5a557c60f2c9e528b1db8649122e545080df09
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2023-01-18 07:31:04 -0300

    :white_check_mark: FootballTeamTest
    test basic constructor

--- src/test/java/org/example/ch04/FootballTeamTest.java
+++ src/test/java/org/example/ch04/FootballTeamTest.java
@@ -1,15 +1,27 @@
 package org.example.ch04;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class FootballTeamTest {
-    @Test
-    void constructorShouldSetGamesWon() {
-        FootballTeam team = new FootballTeam(3);
+    private static final int THREE_GAMES_WON = 3;
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 3, 10})
+    void constructorShouldSetGamesWon(int nbOfGamesWon) {
+        FootballTeam team = new FootballTeam(nbOfGamesWon);
 
         assertThat(team.getGamesWon())
                 .as("number of games won")
-                .isEqualTo(3);
+                .isEqualTo(nbOfGamesWon);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-10, -1})
+    void constructorShouldThrowExceptionForIllegalGamesNb(int illegalNbOfGames) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {new FootballTeam(illegalNbOfGames);});
     }
 }
