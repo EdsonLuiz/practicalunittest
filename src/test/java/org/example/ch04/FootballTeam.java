dfaf7ed2ad4bf78372e5888683a6f01ef7e6aead
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2023-01-18 07:30:02 -0300

    :sparkles: FootballTeamTest
    basic constructor

--- src/test/java/org/example/ch04/FootballTeam.java
+++ src/test/java/org/example/ch04/FootballTeam.java
@@ -1,11 +1,14 @@
 package org.example.ch04;
 
 public class FootballTeam {
+    private int gamesWon;
     public FootballTeam(int gamesWon) {
-        
+        if(gamesWon < 0)
+            throw new IllegalArgumentException("Not possible to have less than 0 games won! (was " + gamesWon + ")");
+        this.gamesWon = gamesWon;
     }
 
     public int getGamesWon() {
-        return 0;
+        return gamesWon;
     }
 }
